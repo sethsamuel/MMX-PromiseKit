a772bc946f5d6cee92d31180f66d4d616a8d2616
Seth Samuel <sethfsamuel@gmail.com>
2015-08-26 18:30:39 -0400

    Initial commit

---
+++ MMX-PromiseKit/MMX-PromiseKit.h
@@ -0,0 +1,19 @@
+//
+//  MMX-PromiseKit.h
+//  MMX-PromiseKit
+//
+//  Created by Seth on 8/26/15.
+//  Copyright (c) 2015 Seth Samuel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MMX-PromiseKit.
+FOUNDATION_EXPORT double MMX_PromiseKitVersionNumber;
+
+//! Project version string for MMX-PromiseKit.
+FOUNDATION_EXPORT const unsigned char MMX_PromiseKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MMX_PromiseKit/PublicHeader.h>
+
+
---
+++ MMX-PromiseKitTests/MMX_PromiseKitTests.swift
@@ -0,0 +1,36 @@
+//
+//  MMX_PromiseKitTests.swift
+//  MMX-PromiseKitTests
+//
+//  Created by Seth on 8/26/15.
+//  Copyright (c) 2015 Seth Samuel. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class MMX_PromiseKitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
