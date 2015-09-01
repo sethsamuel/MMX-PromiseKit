96a17d876097e1a0ed2cb9364b7e355a69c50df8
Seth Samuel <sethfsamuel@gmail.com>
2015-09-01 00:29:49 -0400

    First swift extension

---
+++ MMX-PromiseKit/Set+MMX_PromiseKit.swift
@@ -0,0 +1,20 @@
+//
+//  Set+MMX_PromiseKit.swift
+//  MMX-PromiseKit
+//
+//  Created by Seth on 8/31/15.
+//  Copyright (c) 2015 Seth Samuel. All rights reserved.
+//
+
+import Foundation
+
+extension Set{
+    func castToNSObject() -> Set<NSObject>{
+        var s = Set<NSObject>()
+        for x in self{
+            s.insert(x as! NSObject)
+        }
+        return s
+    }
+    
+}
---
+++ MMX-PromiseKitTests/MMX-PromiseKit.swift
@@ -0,0 +1,40 @@
+//
+//  MMX-PromiseKit.swift
+//  MMX-PromiseKit
+//
+//  Created by Seth on 9/1/15.
+//  Copyright (c) 2015 Seth Samuel. All rights reserved.
+//
+
+import Foundation
+import MMX
+import MMX_PromiseKit
+import OCMock
+import XCTest
+
+class MMX_PromiseKit : XCTestCase{
+    
+    func testFindByTags(){
+        class MockMMXChannel : MMXChannel {
+            override class func findByTags(tags: Set<NSObject>!, success: ((Int32, [AnyObject]!) -> Void)!, failure: ((NSError!) -> Void)!) -> Void{
+                let stubChannels = ["stub"]
+                success(2, stubChannels)
+            }
+
+        }
+
+        let ex = expectationWithDescription("findByTags")
+
+        MockMMXChannel.findByTags(["foo"])
+            .then{ count, channels -> Void in
+                XCTAssertEqual(count, 2, "Count equal")
+                let stubChannels = ["stub"]
+                XCTAssertEqual(channels, stubChannels, "Channels equal")
+                ex.fulfill()
+        }
+        
+        waitForExpectationsWithTimeout(2, handler: nil)
+    }
+    
+}
+
